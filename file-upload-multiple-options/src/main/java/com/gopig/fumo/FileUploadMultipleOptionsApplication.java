a3a5ed95a52f95f5d39337a06cfac5ad1d9a1967
gopin <gopin@GOPI-INDIA>
2021-07-05 02:04:40 +0530

    Initial Commit

---
+++ file-upload-multiple-options/src/main/java/com/gopig/fumo/FileUploadMultipleOptionsApplication.java
@@ -0,0 +1,13 @@
+package com.gopig.fumo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileUploadMultipleOptionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FileUploadMultipleOptionsApplication.class, args);
+	}
+
+}
---
+++ file-upload-multiple-options/src/test/java/com/gopig/fumo/FileUploadMultipleOptionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gopig.fumo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileUploadMultipleOptionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
